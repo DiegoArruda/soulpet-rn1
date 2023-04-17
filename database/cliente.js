ed3b30f8587a1c624da53a0f997289b782cb8536
Diego Arruda <arrudadiego1@gmail.com>
2023-04-17 16:52:57 -0300

    Relação Cliente-Endereço

---
+++ database/cliente.js
@@ -0,0 +1,32 @@
+//Modelo para gerar a tabela de clientes no MySQL
+//Mapeamento: cada propriedade vira uma colunda da tabela
+
+//DataTypes = serve para definir qual o tipo da coluna
+const { DataTypes } = require("sequelize");
+const { connection } = require("./database");
+
+const Cliente = connection.define("cliente", {
+  nome: {
+    // nome VARCHAR NOT NULL
+    // Configurar a coluna 'nome'
+    type: DataTypes.STRING,
+    allowNull: false, // NOT FULL
+  },
+  email: {
+    // email VARCHAR UNIQUE NOT NULL
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  telefone: {
+    // telefone VARCHAR NOT NULL
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+const Endereço = require("./endereco");
+
+Cliente.hasOne(Endereço);
+Endereço.belongsTo(Cliente);
+
+module.exports = Cliente;
--- index.js
+++ index.js
@@ -9,8 +9,30 @@ app.use(express.json()); //Possibilitar transitar dados usando JSON
 //Configuração do Banco de Dados
 const { connection, authenticate } = require("./database/database");
 authenticate(connection);
+const Cliente = require("./database/cliente");
+const Endereco = require("./database/endereco");
 
 //Definição de rotas
+app.post("/clientes", async (req, res) => {
+  // - Coletar informação do req.body;
+  const { nome, email, telefone, endereco } = req.body;
+
+  try {
+    // Dentro de 'novo' estará o objeto criado
+    const novo = await Cliente.create(
+      { nome, email, telefone, endereco },
+      { include: [Endereco] }
+    );
+    res.status(201).json(novo);
+  } catch (err) {
+    res.status(500).json({ message: "Um erro aconteceu." });
+  }
+});
 
 //Escuta de eventos (listen)
-app.listen("3000", (res, rep) => {});
+app.listen(4000, () => {
+  // Gerar as tabelas a partir do model
+  //Force = apaga tudo e recria as tabelas
+  connection.sync({ force: true });
+  console.log("http://localhost:3000/");
+});
