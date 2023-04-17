67ab249a5d05864e076363cbbeddbd4ff30901c3
Diego Arruda <arrudadiego1@gmail.com>
2023-04-17 13:27:51 -0300

    Commit 2 - manhã

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 //Vai disponibilizar o uso de variáveis de ambiente
 require("dotenv").config();
+const express = require("express");
+
+//Configuração do App
+const app = express();
+app.use(express.json()); //Possibilitar transitar dados usando JSON
+
+//Configuração do Banco de Dados
+const { connection, authenticate } = require("./database/database");
+authenticate(connection);
+
+//Definição de rotas
+
+//Escuta de eventos (listen)
+app.listen("3000", (res, rep) => {});
